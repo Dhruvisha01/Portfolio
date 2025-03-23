c3fb865d0ff700f6db6b71e95ae0b7df062ebd2f
Dhruvisha Mondhe <63810182+Dhruvisha01@users.noreply.github.com>
2025-03-23 12:32:38 -0400

    Contact Test

--- api/contact.js
+++ api/contact.js
@@ -2,32 +2,34 @@
 import nodemailer from 'nodemailer';
 
 export default async function handler(req, res) {
-  if (req.method !== 'POST') {
-    return res.status(405).send({ message: 'Only POST allowed' });
-  }
-
-  const { name, email, subject, message } = req.body;
-
-  const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: 'dhruvishamondhe01@gmail.com',
-      pass: 'ixyqqjxgakhptqks'
+    console.log("From contact API", req.body);
+    if (req.method !== 'POST') {
+        return res.status(405).send({ message: 'Only POST allowed' });
     }
-  });
 
-  const mailOptions = {
-    from: email,
-    to: 'dhruvishamondhe01@gmail.com',
-    subject: `New message from ${name}: ${subject}`,
-    text: `You received a message:\n\nFrom: ${name} <${email}>\n\nMessage:\n${message}`
-  };
+    const { name, email, subject, message } = req.body;
 
-  try {
-    await transporter.sendMail(mailOptions);
-    res.status(200).json({ success: true });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ success: false, error: err.message });
-  }
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'dhruvishamondhe01@gmail.com',
+            pass: process.env.GMAIL_APP_PASSWORD
+            // pass: 'ixyqqjxgakhptqks'
+        }
+    });
+
+    const mailOptions = {
+        from: email,
+        to: 'dhruvishamondhe01@gmail.com',
+        subject: `New message from ${name}: ${subject}`,
+        text: `You received a message:\n\nFrom: ${name} <${email}>\n\nMessage:\n${message}`
+    };
+
+    try {
+        await transporter.sendMail(mailOptions);
+        res.status(200).json({ success: true });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ success: false, error: err.message });
+    }
 }
